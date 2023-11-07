c95bc14235fb9c1b99d7ece7bb65566136d6b2d2
Arthur <arturo.kudabaev@mail.ru>
2023-11-07 23:30:01 +0500

    Burger menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(function () {
 
+  setInterval(() => {
+    if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+      $('.burger').addClass('burger--follow')
+    } else {
+      $('.burger').removeClass('burger--follow')
+    }
+  }, 0)
+  $('.burger, .overlay, .header__top button').on('click', function (e) {
+    e.preventDefault()
+    $('.header__top').toggleClass('header__top--open')
+    $('.overlay').toggleClass('overlay--show')
+    $('.burger').toggleClass('active');
+  })
+
   const rating = 4.5;
 
   let number = document.querySelector('.header__rating-number');
